d08f26c3df1f4c5e2cdead5b20565e27fa0a6538
vinupeeks <vinupeeks@gmail.com>
2026-03-15 17:10:02 +0530

    fix workflow

--- scripts/update.js
+++ scripts/update.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
 
-const filePath = "src/autoUpdate.txt";
-
+const file = "src/autoUpdate.txt";
 const time = new Date().toLocaleString();
 
-const content = `Update: ${time}\n`;
+fs.appendFileSync(file, `Update: ${time}\n`);
 
-fs.appendFileSync(filePath, content);
-
-console.log("File updated");
+console.log("Updated:", time);
